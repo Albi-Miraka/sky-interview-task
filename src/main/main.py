1f05e9ed6f76a789f33517f1914fe5d494f37d1d
AlbiMiraka <mirakalbi@gmail.com>
2024-06-19 21:49:59 +0200

    Complete Task

--- src/main/main.py
+++ src/main/main.py
@@ -1,21 +1,23 @@
+import logging
 import sys
 import time
 from typing import List
+
 import requests
-import logging
 from requests.auth import HTTPBasicAuth
+
 from src.main import Result
 from src.main.Result import filter_result_by_active_data_and_video_format_and_endpoint_origin_level, \
     print_title_name_medium, filter_result_by_active, filter_result_by_can_be_played_on_ROKU, print_endpoints, Endpoint
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d-%m-%y %H:%M', level=logging.INFO)
 logging.info("Starting execution")
 
 NUMBER_OF_ITERATIONS = 3
 PRINT_FILTER_BY_DEVICE = False
 PRINT_FILTER_BY_DEVICE_AND_ACTIVE = False
 
-if sys.argv.__len__() != 0:
+if sys.argv.__len__() > 1:
     args_map: dict = {}
     for index in range(1, 3):
         if sys.argv[index] is not None and sys.argv[index].__contains__("="):
@@ -60,8 +62,13 @@ if iterations > NUMBER_OF_ITERATIONS or ((result_right is None or result_right.s
     logging.info("Arresting execution")
     sys.exit(1)
 
-response_asset = Result.Response.from_dict(result_asset.json(), Result.ResultType.ASSET)
-response_right = Result.Response.from_dict(result_right.json(), Result.ResultType.RIGHT)
+try:
+    response_asset = Result.Response.from_dict(result_asset.json(), Result.ResultType.ASSET)
+    response_right = Result.Response.from_dict(result_right.json(), Result.ResultType.RIGHT)
+except Exception as e:
+    logging.error(e)
+    logging.info("Arresting execution")
+    sys.exit(1)
 
 filtered_data_by_device: List[Result.ResultRights] = filter_result_by_can_be_played_on_ROKU(response_right.results)
 if PRINT_FILTER_BY_DEVICE:
@@ -72,11 +79,10 @@ if PRINT_FILTER_BY_DEVICE_AND_ACTIVE:
     print_title_name_medium(filtered_data_by_device_and_active,
                             "Active rights of TV shows/movies title that can be played on ROKU:")
 
+list_of_active_content_id: List[str] = [result.content_id for result in filter_result_by_active(response_right.results)]
 filtered_endpoints_by_active_and_video_format_and_origin: List[Endpoint] = (
     filter_result_by_active_data_and_video_format_and_endpoint_origin_level(response_asset.results,
-                                                                            [result.content_id for result in
-                                                                             filter_result_by_active(
-                                                                                 response_right.results)]))
+                                                                            list_of_active_content_id))
 print_endpoints(filtered_endpoints_by_active_and_video_format_and_origin, "Manifests of active endpoints:")
 
 logging.info("End of commands")
